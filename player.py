3b2d4023f6f1df64dc2e4d5049a1e0ff237558d6
GuillemotClement <guillemot.clement@protonmail.com>
2025-01-01 13:42:48 +0100

    Fix sense rotate

--- player.py
+++ player.py
@@ -24,6 +24,6 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_q]:
-            self.rotate(dt)
-        if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
